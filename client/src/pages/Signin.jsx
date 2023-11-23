191b6f15d329428519d0f6a7b305278c81088c8c
danemaas <danemaas16@gmail.com>
2023-11-23 23:56:38 +0800

    created ui for signin and signup

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,10 +1,70 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { EyeIcon, EyeOff } from "lucide-react";
 
 const Signin = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [userDetails, setUserDetails] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
   return (
-    <div>
-      <h2>Sign In</h2>
-    </div>
+    <section
+      className="w-full min-h-screen flex flex-col items-center
+    bg-slate-200"
+    >
+      <h2 className="pt-10 text-2xl font-medium mb-5">Sign In</h2>
+      <form
+        action=""
+        className="w-full max-w-[320px] md:max-w-[400px]
+        flex flex-col gap-3 p-2 rounded-md"
+      >
+        <div className="flex flex-col w-full">
+          <label>Email</label>
+          <input
+            type="email"
+            className="p-2 rounded-md border-2 outline-none"
+          />
+        </div>
+        <div className="flex flex-col w-full relative">
+          <label>Password</label>
+          <input
+            type={isVisible ? "text" : "password"}
+            className="p-2 rounded-md border-2 outline-none"
+          />
+          <div className="absolute right-3 bottom-3 cursor-pointer">
+            {isVisible ? (
+              <EyeIcon onClick={() => setIsVisible(false)} size={20} />
+            ) : (
+              <EyeOff onClick={() => setIsVisible(true)} size={20} />
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col w-full gap-3">
+          <button
+            className="bg-slate-500 p-2 uppercase text-white disabled:opacity-50
+            rounded-md font-medium hover:scale-105 shadow-sm shadow-black
+            transition-all duration-300"
+          >
+            sign in
+          </button>
+          <button
+            className="border-2 p-2 uppercase rounded-md hover:bg-cyan-500
+            hover:text-slate-800 transition-all duration-300"
+          >
+            continue with google
+          </button>
+        </div>
+        <p className="mt-5">
+          Still don't have an account?{" "}
+          <Link to="/sign-up" className="text-cyan-500 font-medium">
+            Sign Up
+          </Link>
+        </p>
+      </form>
+    </section>
   );
 };
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,10 +1,87 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { EyeIcon, EyeOff } from "lucide-react";
 
 const Signup = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [userDetails, setUserDetails] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
   return (
-    <div>
-      <h2>Sign Up</h2>
-    </div>
+    <section className="w-full min-h-screen flex flex-col items-center bg-slate-200">
+      <h2 className="pt-10 text-2xl font-medium mb-5">Sign Up</h2>
+      <form
+        action=""
+        className="w-full max-w-[320px] md:max-w-[400px]
+        flex flex-col gap-3 p-2 rounded-md"
+      >
+        <div className="flex flex-col w-full">
+          <label>Username</label>
+          <input
+            type="text"
+            value={userDetails.username}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, username: e.target.value })
+            }
+            className="p-2 rounded-md border-2
+            outline-none "
+          />
+        </div>
+        <div className="flex flex-col w-full">
+          <label>Email</label>
+          <input
+            type="email"
+            value={userDetails.email}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, email: e.target.value })
+            }
+            className="p-2 rounded-md border-2 outline-none"
+          />
+        </div>
+        <div className="flex flex-col w-full relative">
+          <label>Password</label>
+          <input
+            type={isVisible ? "text" : "password"}
+            value={userDetails.password}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, password: e.target.value })
+            }
+            className="p-2 rounded-md border-2 outline-none"
+          />
+          <div className="absolute right-3 bottom-3 cursor-pointer">
+            {isVisible ? (
+              <EyeIcon onClick={() => setIsVisible(false)} size={20} />
+            ) : (
+              <EyeOff onClick={() => setIsVisible(true)} size={20} />
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col w-full gap-3">
+          <button
+            className="bg-slate-500 p-2 uppercase text-white disabled:opacity-50
+            rounded-md font-medium hover:scale-105 shadow-sm shadow-black
+            transition-all duration-300"
+          >
+            sign up
+          </button>
+          <button
+            className="border-2 p-2 uppercase rounded-md hover:bg-cyan-500
+            hover:text-slate-800 transition-all duration-300"
+          >
+            continue with google
+          </button>
+        </div>
+        <p className="mt-5">
+          Already have an account?{" "}
+          <Link to="/sign-in" className="text-cyan-500 font-medium">
+            Sign In
+          </Link>
+        </p>
+      </form>
+    </section>
   );
 };
 
