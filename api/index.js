9e10f9c268a9a9c65f36b72a54b43073a3f8e209
danemaas <danemaas16@gmail.com>
2023-11-23 22:15:17 +0800

    added and connected to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("CONNECTED TO MongoDB!");
+  })
+  .catch((err) => {
+    console.log("Error connecting to DB: ", err);
+  });
 const app = express();
 
 app.listen(3000, () => {
