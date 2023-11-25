8b548efc4ffd55d93ea1981619ed6c045a17ebaa
danemaas <danemaas16@gmail.com>
2023-11-25 14:43:40 +0800

    added create listing api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
 
 dotenv.config();
 mongoose
@@ -17,15 +19,15 @@ mongoose
 
 const app = express();
 
-app.use(cookieParser());
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
